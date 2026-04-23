0fe756c61f124809642fe017bbcfb6e94ce64ec3
José Campos <127572595+N00bunaga@users.noreply.github.com>
2026-04-22 22:02:57 -0500

    Add files via upload
    Prueba para el caso de un solo momento angular como generador

--- SimuNucl1_ShellModel.py
+++ SimuNucl1_ShellModel.py
@@ -23,45 +23,36 @@ def Cl_Go_CAL(j1,j2,J,m1,m2,M):
 def KDelta(i,j):
     return np.equal(i,j).astype(int)
 
+import numpy as np
+from sympy.physics.wigner import wigner_6j
 
-def CFP_3Cuerpos(j,J): #j el m. angular para particulas identicas, J el resultado
-    from sympy.physics.wigner import wigner_6j
-    J = np.atleast_1d(J)  # asegura array
-    J12 = np.arange(0,2*j,2) #Estados intermedios o progenitor (jj)
-    J12P = np.random.choice(J12) #Generador escogido aleatoriamente
-    CFP_as = np.zeros(shape=(len(J12),len(J)), dtype=float) #Genero memoria
-    #Calculo de los 6j (como matriz o elemento de)
-    w6j = np.zeros((len(J12), len(J)), dtype=float)
-    for a, J12_i in enumerate(J12):
-        for b, J_i in enumerate(J):
-            w6j[a, b] = float(wigner_6j(j, j, J12P, J_i, j, J12_i))
-    delta = KDelta(J12P, J12)[:, None]
-    factor = 2*np.sqrt(2*J12P + 1) * np.sqrt(2*J12)[:, None]
-    ###
-    CFP_as = delta + factor*w6j
+def KDelta(i,j):
+    return np.equal(i,j).astype(int)
 
-    #Normalización metodo 1 (Analitico)
-    CFP_nas1 = np.zeros_like(CFP_as)
-    CFP_nas2 = np.zeros_like(CFP_as)
-    for b, J_i in enumerate(J):
-        #Metodo 1 (Analítico (depende de J))
-        w6j_diag = float(wigner_6j(j, j, J12P, J_i, j, J12P))
-        denom = 3*(1 + 2*(2*J12P + 1)*w6j_diag)
-        if not np.isclose(denom, 0):
-            Norm1 = denom**(-0.5)
-            CFP_nas1[:, b] = Norm1 * CFP_as[:, b]
-        else:
-            CFP_nas1[:, b] = np.nan  # estado no físico
+def CFP_3Cuerpos(j,J):
+    J12 = np.arange(0, 2*j+1, 2)
+    J12P = 4 #np.random.choice(J12)
 
-        #Metodo 2 (Numerico)
-        norm2 = np.sqrt(np.sum(CFP_as[:, b]**2))
-        if norm2 > 1e-12:
-            CFP_nas2[:, b] = CFP_as[:, b] / norm2
-        else:
-            CFP_nas2[:, b] = np.nan
-    print("Norma analítica:", np.sum(CFP_nas1[:, b]**2))
-    print("Norma numérica:", np.sum(CFP_nas2[:, b]**2))
-    return [CFP_nas1,"*",CFP_nas2], J12, J12P
-E = np.array([3/2,5/2,9/2]) #Los momentos angulares de prueba en el libro
-print(CFP_3Cuerpos(5/2,E))
-#print(CFP_3Cuerpos(5/2,3/2))
+    # 6j para cada J12
+    w6j_vals = np.array([
+        float(wigner_6j(j, j, J12P, J, j, J12_i))
+        for J12_i in J12
+    ])
+
+    CFP_as = (
+        KDelta(J12P, J12)
+        + 2*np.sqrt(2*J12P + 1)*np.sqrt(2*J12 + 1)*w6j_vals
+    )
+
+    # 6j diagonal
+    w6j_diag = float(wigner_6j(j, j, J12P, J, j, J12P))
+
+    # normalización analítica
+    Norm = (3*(1 + 2*(2*J12P + 1)*w6j_diag))**(-0.5)
+
+    CFP = Norm * CFP_as
+
+    return CFP, J12, J12P
+j = 5/2
+J = 3/2
+print(CFP_3Cuerpos(j,J))
